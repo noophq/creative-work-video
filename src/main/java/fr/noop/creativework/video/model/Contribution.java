9d77f42b8408f57dde8c40732124a22f620d1f46
Cyrille Lebeaupin <clebeaupin@noop.fr>
2016-04-15 15:45:10 +0200

    Allow json serialization of contributors and films

--- src/main/java/fr/noop/creativework/video/model/Contribution.java
+++ src/main/java/fr/noop/creativework/video/model/Contribution.java
@@ -1,10 +1,12 @@
 package fr.noop.creativework.video.model;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import fr.noop.creativework.video.Activity;
 
-/**
- * Created by clebeaupin on 18/12/15.
- */
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.CLASS,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "@class")
 public interface Contribution {
     public Work getWork();
     public Contributor getContributor();
--- src/main/java/fr/noop/creativework/video/model/Contributor.java
+++ src/main/java/fr/noop/creativework/video/model/Contributor.java
@@ -1,10 +1,13 @@
 package fr.noop.creativework.video.model;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import java.util.List;
 
-/**
- * Created by clebeaupin on 18/12/15.
- */
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.CLASS,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "@class")
 public interface Contributor {
     public List<Contribution> getContributions();
 }
